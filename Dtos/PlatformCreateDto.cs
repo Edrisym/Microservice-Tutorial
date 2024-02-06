1b1675b4ffe04836a502b8f5806024263690a453
ادریس غفوری <edrismoavenghafouri@gmail.com>
2024-02-06 10:56:39 +0330

    Adding Dtos

---
+++ Dtos/PlatformCreateDto.cs
@@ -0,0 +1,8 @@
+namespace PlatformReadDtoService.Dtos;
+
+public class PlatformCreateDto
+{
+    public string Name { get; set; }
+    public string Publisher { get; set; }
+    public string Cost { get; set; }
+}
---
+++ Dtos/PlatformReadDto.cs
@@ -0,0 +1,9 @@
+namespace PlatformReadDtoService.Dtos;
+
+public class PlatformReadDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Publisher { get; set; }
+    public string Cost { get; set; }
+}
