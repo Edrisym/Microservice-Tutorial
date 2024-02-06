30a40c6d7a863445ab00f1cf31bd6ba5aa70a34d
ادریس غفوری <edrismoavenghafouri@gmail.com>
2024-02-06 11:04:12 +0330

    Adding AutoMapper,DbContext,DI To the Program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<AppDbContext>(option =>
+                                            option.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 
 var app = builder.Build();
 
@@ -22,4 +28,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+PrepDb.PrepPopulation(app);
+
 app.Run();
+
