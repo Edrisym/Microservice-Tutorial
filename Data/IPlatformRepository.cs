1d2eadce7ee05a5de2303019522e562fb943266b
ادریس غفوری <edrismoavenghafouri@gmail.com>
2024-02-06 10:56:14 +0330

    Adding repositories

---
+++ Data/IPlatformRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepository
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
---
+++ Data/PrepDb.cs
@@ -0,0 +1,32 @@
+
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+        }
+    }
+
+    private static void SeedData(AppDbContext context)
+    {
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine("---> Seeding database...");
+            context.Platforms.AddRange(
+           new Platform() { Name = "dotnet", Publisher = "Microsoft", Cost = "Free" },
+           new Platform() { Name = "Edris", Publisher = "Bluh Bluh Bluh", Cost = "Expensive" });
+
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("---> We already have database");
+        }
+    }
+}
