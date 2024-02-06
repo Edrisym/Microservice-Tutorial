059de0d37649cee308f6bb729be948e6cf30cdae
ادریس غفوری <edrismoavenghafouri@gmail.com>
2024-02-06 11:08:08 +0330

    Adding the platform controller

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using PlatformReadDtoService.Dtos;
+using PlatformService.Data;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepository _platformRepository;
+    private readonly IMapper _mapper;
+    public PlatformsController(IMapper mapper, IPlatformRepository platformRepository)
+    {
+        _platformRepository = platformRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatfroms()
+    {
+        Console.WriteLine("Getting platforms");
+        var platformItems = _platformRepository.GetAllPlatforms();
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+    }
+}
