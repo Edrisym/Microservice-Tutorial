03f83c0c46c77b670b4183cac0d115e1d7f9c1f2
Edris <edrismoavenghafouri@gmail.com>
2024-02-29 16:41:55 +0330

    just a push

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -3,7 +3,7 @@ using Platform.SyncDataServices.Http;
 using PlatformService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//
 // Add services to the container.
 
 builder.Services.AddControllers();
